0ee726e4d3daf0b7d9754d67de016b0411ff8af0
manh <ngomanh.dev@gmail.com>
2020-11-18 11:19:22 +0700

    NMCode_edit_manager/18:11:2020 11:18/exchange size dialog

--- app/src/main/java/com/example/audiocutter/activities/acttest/testnm/TestAct.kt
+++ app/src/main/java/com/example/audiocutter/activities/acttest/testnm/TestAct.kt
@@ -3,40 +3,20 @@ package com.example.audiocutter.activities.acttest.testnm
 import android.os.Bundle
 import com.example.audiocutter.R
 import com.example.audiocutter.base.BaseActivity
-import com.example.audiocutter.core.manager.ManagerFactory
-import com.example.audiocutter.functions.audiochooser.dialogs.DialogAppShare
-import com.example.audiocutter.objects.AudioFile
-import kotlinx.android.synthetic.main.act_test.*
+import com.example.audiocutter.functions.settings.screens.SettingScreens
 import java.io.File
 
 
 class TestAct : BaseActivity() {
     val path = "/storage/emulated/0/AudioCutter/mixer/aloha - Copy (4).mp3"
     val file = File(path)
-    private lateinit var dialog: DialogAppShare
-    private var audioFile: AudioFile = AudioFile(
-        file,
-        "hello",
-        12588L,
-        128,
-        1255L,
-        ManagerFactory.getAudioFileManager().getUriByPath(
-            file
-        )!!,
-        null,
-        "",
-        "",
-        "", "",
-        "",
-        ".mp3"
-    )
+    private lateinit var frg: SettingScreens
 
 
     override fun createView(savedInstanceState: Bundle?) {
         setContentView(R.layout.act_test)
-        bt_start.setOnClickListener {
-            ManagerFactory.getAudioFileManager().openWithApp(audioFile.uri!!)
-        }
+        frg = SettingScreens()
+        supportFragmentManager.beginTransaction().replace(R.id.ln_main, frg).commit()
     }
 
 
---
+++ app/src/main/java/com/example/audiocutter/functions/settings/dialogs/SetLanguageDialog.kt
@@ -0,0 +1,80 @@
+package com.example.audiocutter.functions.settings.dialogs
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import com.example.audiocutter.R
+
+class SetLanguageDialog(val mContext: Context) : DialogFragment(), View.OnClickListener {
+    private lateinit var rootView: View
+    private lateinit var rbVietNam: RadioButton
+    private lateinit var rbEnglish: RadioButton
+    private lateinit var tvCancel: TextView
+    private lateinit var tvOK: TextView
+    private lateinit var mCallBack: DialogSettingsListener
+    private var indexItem = 0
+
+
+    fun setOnCallBack(event: DialogSettingsListener) {
+        mCallBack = event
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        rootView = LayoutInflater.from(mContext).inflate(R.layout.dialog_language, container, false)
+        initViews()
+        return rootView
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.DialogGray)
+
+    }
+
+
+    fun initViews() {
+
+        rbVietNam = rootView.findViewById(R.id.rb_vietnam)
+        rbEnglish = rootView.findViewById(R.id.rb_english)
+        tvCancel = rootView.findViewById(R.id.tv_cancel_dialog_setting_language)
+        tvOK = rootView.findViewById(R.id.tv_ok_dialog_setting_language)
+
+        rbEnglish.setOnClickListener(this)
+        rbVietNam.setOnClickListener(this)
+        tvOK.setOnClickListener(this)
+        tvCancel.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            rbEnglish -> {
+                indexItem = 1
+            }
+            rbVietNam -> {
+                indexItem = 0
+            }
+            tvOK -> {
+                mCallBack.setLanguage(indexItem)
+                dismiss()
+            }
+            tvCancel -> {
+                dismiss()
+            }
+        }
+    }
+
+    interface DialogSettingsListener {
+        fun setLanguage(item: Int)
+    }
+
+}
