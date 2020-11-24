db849b361cb3e65e7253b4ef6fb0ac631302ec01
manh <ngomanh.dev@gmail.com>
2020-11-24 10:55:43 +0700

    NMCode_edit_manager/24:11:2020 10:52/done layout flash_call_screen.xml

--- app/src/main/java/com/example/audiocutter/functions/flashcall/sreens/FlashCallSceen.kt
+++ app/src/main/java/com/example/audiocutter/functions/flashcall/sreens/FlashCallSceen.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.databinding.DataBindingUtil
 import com.example.audiocutter.R
 import com.example.audiocutter.base.BaseFragment
 import com.example.audiocutter.databinding.FlashCallScreenBinding
 
-class FlashCallSceen : BaseFragment() {
+class FlashCallSceen : BaseFragment(), CompoundButton.OnCheckedChangeListener {
     private lateinit var binding: FlashCallScreenBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -19,6 +20,17 @@ class FlashCallSceen : BaseFragment() {
     }
 
     private fun initViews() {
+        binding.swFlashCallMode.setOnCheckedChangeListener(this)
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        if(isChecked){
+            binding.lnFlashOffMode.visibility=View.INVISIBLE
+            binding.lnFlashOnMode.visibility=View.VISIBLE
+        }else{
+            binding.lnFlashOffMode.visibility=View.VISIBLE
+            binding.lnFlashOnMode.visibility=View.INVISIBLE
+        }
     }
 
 }
