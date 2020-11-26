89d7b81a94542173664300cd2f4166b2fe999785
manh <ngomanh.dev@gmail.com>
2020-11-26 18:15:43 +0700

    NMCode_FlashCall_screens/25:11:2020 18:15/ edit flashcall stop- notification

--- app/src/main/java/com/example/audiocutter/functions/audiochooser/screens/MergePreviewScreen.kt
+++ app/src/main/java/com/example/audiocutter/functions/audiochooser/screens/MergePreviewScreen.kt
@@ -2,7 +2,6 @@ package com.example.audiocutter.functions.audiochooser.screens
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
