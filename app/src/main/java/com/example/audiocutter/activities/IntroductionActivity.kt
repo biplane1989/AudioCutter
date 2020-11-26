e66f585dea80e1a7a34919a423cb4c2df9fd2071
taihoang0895 <48268462+taihoang0895@users.noreply.github.com>
2020-11-26 13:34:10 +0700

    taih_introduction_func 26/11/2020 1:34pm doing

---
+++ app/src/main/java/com/example/audiocutter/activities/IntroductionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.audiocutter.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.audiocutter.R
+
+class IntroductionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduction)
+    }
+}
